fedff3683f8b6660a1621e514d8cbb3eb070fa80
Soeun Park <soeunpark0806@gmail.com>
2025-03-18 16:10:49 +0900

    IhateBigKii

--- src/App.js
+++ src/App.js
@@ -1,99 +1,155 @@
-function ProductCategoryRow({ category}) {
-  return(
-    <tr>
-      <th colSpan="2">
-        {category}
-      </th>
-    </tr>
-  );
-}
-
-function ProductRow({ product }) {
-  const name = product.stocked ? product.name : 
-    <span style={{ color : 'red'}}>
-      {product.name}
-    </span>;
-
+function Profile() {
   return (
-    <tr>
-      <td>{name}</td>
-      <td>{product.price}</td>
-    </tr>
+    <img
+    src="https://i.imgur.com/MK3eW3As.jpg"
+    alt="Katherine Johnson"
+    />
   );
 }
 
-function ProductTable({ products }) {
-  const rows = [];
-  let lastCategory = null;
-
-  products.forEach((product) => {
-    if (product.category !== lastCategory) {
-      rows.push(
-        <ProductCategoryRow
-          category={product.category}
-          key={product.category} />
-      );
-    }
-    rows.push(
-      <ProductRow
-        product={product}
-        key={product.name} />
-    );
-    lastCategory = product.category;
-  });
-
+export default function Gallary() {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Price</th>
-        </tr>
-      </thead>
-      <tbody>{rows}</tbody>
-    </table>
+    <section>
+      <h1>Amazing Scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
   );
 }
 
-function SearchBar() {
-  return (
-    <form>
-      <input type="text" placeholder="Search..." />
-      <label>
-        <input type="checkbox" />
-        {' '}
-        Only show products in stock
-      </label>
-    </form>
-  );
-}
-
-function FilterableProductTable({ products }) {
-  return (
-    <div>
-      <SearchBar />
-      <ProductTable products={products} />
-    </div>
-  );
-}
-
-const PRODUCTS = [
-  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
-  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
-  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
-  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
-  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
-  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
-];
-
-export default function App() {
-  return <FilterableProductTable products={PRODUCTS} />; //이게뭐지 
-}
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+// 인사하기
+// import { useState } from "react";
+
+// function Greeting({ name }) {
+//   return <h1>Greetng, {name}</h1>;
+// }
+
+// export default function App() {
+//   const [name, setName] = useState("world");
+
+//   return (
+//     <div>
+//       <input
+//         type="text"
+//         value={name}
+//         onChange={(e) => setName(e.target.value)}
+//         placeholder="Enter your name"
+//       />
+//       <Greeting name={name} />
+//     </div>
+//   );
+// }
+
+// 카테고리 생성 놀이
+// function ProductCategoryRow({ category}) {
+//   return(
+//     <tr>
+//       <th colSpan="2">
+//         {category}
+//       </th>
+//     </tr>
+//   );
+// }
+
+// function ProductRow({ product }) {
+//   const name = product.stocked ? product.name : 
+//     <span style={{ color : 'red'}}>
+//       {product.name}
+//     </span>;
+
+//   return (
+//     <tr>
+//       <td>{name}</td>
+//       <td>{product.price}</td>
+//     </tr>
+//   );
+// }
+
+// function ProductTable({ products }) {
+//   const rows = [];
+//   let lastCategory = null;
+
+//   products.forEach((product) => {
+//     if (product.category !== lastCategory) {
+//       rows.push(
+//         <ProductCategoryRow
+//           category={product.category}
+//           key={product.category} />
+//       );
+//     }
+//     rows.push(
+//       <ProductRow
+//         product={product}
+//         key={product.name} />
+//     );
+//     lastCategory = product.category;
+//   });
+
+//   return (
+//     <table>
+//       <thead>
+//         <tr>
+//           <th>Name</th>
+//           <th>Price</th>
+//         </tr>
+//       </thead>
+//       <tbody>{rows}</tbody>
+//     </table>
+//   );
+// }
+
+// function SearchBar() {
+//   return (
+//     <form>
+//       <input type="text" placeholder="Search..." />
+//       <label>
+//         <input type="checkbox" />
+//         {' '}
+//         Only show products in stock
+//       </label>
+//     </form>
+//   );
+// }
+
+// function FilterableProductTable({ products }) {
+//   return (
+//     <div>
+//       <SearchBar />
+//       <ProductTable products={products} />
+//     </div>
+//   );
+// }
+
+// const PRODUCTS = [
+//   {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+//   {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+//   {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+//   {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+//   {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+//   {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+// ];
+
+// export default function App() {
+//   return <FilterableProductTable products={PRODUCTS} />; //이게뭐지 
+// }
 
 
 //틱택토게임만들기 
